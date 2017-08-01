4df9102cd169f4c46a2db39d5940409a27655ff6
Rob Alonzi <alonzi.rob@gmail.com>
2017-07-31 23:48:40 -0400

    Sidebar styles done.

--- src/components/pictureFeed.js
+++ src/components/pictureFeed.js
@@ -1,26 +1,25 @@
 import React, { Component } from "react";
-import ReactModal from 'react-modal';
+import Rodal from 'rodal';
 
+import 'rodal/lib/rodal.css';
 import "./pictureFeed.scss";
 
 export default class PictureFeed extends Component{
 	constructor(props){
 		super(props);
+		this.state = { visible : false }
 
-		this.state = {
-			showModal : false
-		}
-
-		this.handleOpenModal = this.handleOpenModal.bind(this);
-    	this.handleCloseModal = this.handleCloseModal.bind(this);
+		this.show = this.show.bind(this);
+		this.hide = this.hide.bind(this);
 	}
 
-	handleOpenModal() {
-		this.setState({ showModal: true });
+	show(picture) {
+		console.log(picture);
+		this.setState({ visible: true });
 	}
 	
-	handleCloseModal() {
-		this.setState({ showModal: false });
+	hide() {
+		this.setState({ visible: false });
 	}
 
 	renderPictures(){
@@ -32,7 +31,7 @@ export default class PictureFeed extends Component{
 
 		return pictures.map((picture, i) => {
 			return (
-				<div key={i} className="picture-feed-picture">
+				<div onClick={() => this.show(picture)} key={i} className="picture-feed-picture">
 					<img src={picture} />
 				</div>
 			);
@@ -44,20 +43,12 @@ export default class PictureFeed extends Component{
 			<section className="section section-picture-feed">
 				<div className="page-wrap-wide">
 					<div className="picture-feed-container">
-						<button onClick={this.handleOpenModal}>Trigger Modal</button>
 						{ this.renderPictures() }
- 
-						<ReactModal 
-							isOpen={this.state.showModal}
-							contentLabel="onRequestClose Example"
-							onRequestClose={this.handleCloseModal}
-							className="Modal"
-							overlayClassName="Overlay">
-							<p>Modal text!</p>
-							<button onClick={this.handleCloseModal}>Close Modal</button>
-       					</ReactModal>
 					</div>
 				</div>
+				<Rodal className="modal-container" visible={this.state.visible} onClose={this.hide} >
+					<img src="http://via.placeholder.com/300x300" />
+				</Rodal>
 			</section>
 		);
 	}
